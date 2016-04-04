e8e1a1b98b6795c2874708d1d8b43a6c961657f1
Gerold Katzinger <gerold.katzinger@conova.com>
2016-04-04 11:32:40 +0200

    fixed typo in install class

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,4 +1,4 @@
-class conwin_collectwin::install (
+class conwin_collectdwin::install (
   $collectdwin_version = $::conwin_collectdwin::params::collectdwin_version,
   $service_state = $::conwin_collectdwin::params::service_state,
 )inherits ::conwin_collectdwin::params{
