310c16339d574fee94a87e12f246ce0282f090ed
Gerold Katzinger <gerold.katzinger@conova.com>
2016-04-04 11:59:11 +0200

    fixed syntax fail in params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,9 +1,9 @@
 class conwin_collectdwin::params {
 
-  $collectdwin_version = $::conwin_collectdwin::collectdwin_version,
-  $service_state = $::conwin_collectdwin::service_state,
-  $debug_level = $::conwin_collectdwin::debug_level,
-  $config = $::conwin_collectdwin::config,  
-  $config_file_win_perfcounter ='C:\Program Files\Bloomberg LP\CollectdWin\config\WriteHttp.config',
-  $config_file_write_http = 'C:\Program Files\Bloomberg LP\CollectdWin\config\WindowsPerformanceCounter.config',
+  $collectdwin_version = $::conwin_collectdwin::collectdwin_version
+  $service_state = $::conwin_collectdwin::service_state
+  $debug_level = $::conwin_collectdwin::debug_level
+  $config = $::conwin_collectdwin::config
+  $config_file_win_perfcounter ='C:\Program Files\Bloomberg LP\CollectdWin\config\WriteHttp.config'
+  $config_file_write_http = 'C:\Program Files\Bloomberg LP\CollectdWin\config\WindowsPerformanceCounter.config'
 }
