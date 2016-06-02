71d0daf00d33db70a5a5210112000b16a1b3845e
Gerold Katzinger <gerold.katzinger@conova.com>
2016-06-02 11:33:13 +0200

    fixed templates

--- manifests/perfcounter.pp
+++ manifests/perfcounter.pp
@@ -8,13 +8,16 @@ define collectdwin::perfcounter (
   $cd_plugininstance,
   $cd_type,
   $cd_typeinstance,
+){
+ 
+  require ::collectdwin::params
+  
   $config_file_win_perfcounter = $::collectdwin::params::config_file_win_perfcounter, 
-)inherits ::collectdwin::params{
-
-  ## Funktion zum hinzufügen von einem concat fragment zum perfcounter xml
   concat::fragment { $name:
-    target => $config_file_win_perfcounter,
-    order  => '10',
+    target  => $config_file_win_perfcounter,
+    content => regsubst(template('collectdwin/WindowsPerformanceCounter.config.erb'), '\n', "\r\n", 'EMG',)
+    order   => '10',
   }
 
 }
+
