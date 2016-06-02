5816601a799ebc117088db3272d3bb2cd4cb3970
Gerold Katzinger <gerold.katzinger@conova.com>
2016-06-02 12:34:39 +0200

    fix bug

--- manifests/perfcounter.pp
+++ manifests/perfcounter.pp
@@ -15,7 +15,7 @@ define collectdwin::perfcounter (
   $config_file_win_perfcounter = $::collectdwin::params::config_file_win_perfcounter 
   concat::fragment { $name:
     target  => $config_file_win_perfcounter,
-    content => regsubst(template('collectdwin/WindowsPerformanceCounter.config.erb'), '\n', "\r\n", 'EMG',)
+    content => regsubst(template('collectdwin/WindowsPerformanceCounter.config.erb'), '\n', "\r\n", 'EMG'),
     order   => '10',
   }
 
