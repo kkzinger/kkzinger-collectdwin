cb974ad04ce863344b3892d46b7d6aa5d891e83b
Gerold Katzinger <gerold.katzinger@conova.com>
2016-06-08 10:39:22 +0200

    fix,fix

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,7 +8,7 @@ class collectdwin::params {
   $config_file_win_perfcounter = 'C:/Program Files/Bloomberg LP/CollectdWin/config/WindowsPerformanceCounter.config'
   $config_file_general = 'C:/Program Files/Bloomberg LP/CollectdWin/config/CollectdWin.config'
   $plugin_writehttp = $::collectdwin::plugin_writehttp
-  $plugin_amqp = $::collectdwin::plugin_aqmp
+  $plugin_amqp = $::collectdwin::plugin_amqp
   $plugin_console = $::collectdwin::plugin_console
   $plugin_statsd = $::collectdwin::plugin_statsd
   $plugin_winperfcounter = $::collectdwin::plugin_winperfcounter
